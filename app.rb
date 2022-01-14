278e15e9dc652ec1581e97400a934010e4220bf7
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-01-14 17:00:07 -0500

    Completed assignment

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require_relative 'config/environment'
+require_relative 'models/puppy'
 
 class App < Sinatra::Base
+
+   #load homepage with link 
+    get '/' do 
+        erb :index
+    end
+
+    #load homepage with form 
+    get '/new' do 
+        erb :create_puppy
+    end
+
+    #display puppies that are created
+    post '/puppy' do 
+        @puppy = Puppy.new(params[:name],params[:breed] ,params[:age] )
+    
+        erb :display_puppy
+    end
 
 end
